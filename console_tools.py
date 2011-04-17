794efbc20158ea0e8f52d4f0a5ed8787de3fee2d
Dmitriy Grachjev <dvg@post.ru>
2011-04-17 21:34:26 +0400

    pep8 + unit fileformat

--- console_tools.py
+++ console_tools.py
@@ -7,21 +7,24 @@
 import sys
 import codecs
 import copy
+import ctypes
 import logging
 
 ansi = not sys.platform.startswith("win")
 
+
 def setup_console(sys_enc='utf-8', use_colorama=True):
     """
-    Set sys.defaultencoding to `sys_enc` and update stdout/stderr writers to corresponding encoding
+    Set sys.defaultencoding to `sys_enc` and update stdout/stderr writers
+    to corresponding encoding
 
-    .. note:: For Win32 the OEM console encoding will be used istead of `sys_enc`
+    .. note:: For Win32 the OEM console encoding will be used istead
+    of `sys_enc`
     """
     global ansi
     reload(sys)
     try:
         if sys.platform.startswith("win"):
-            import ctypes
             enc = "cp%d" % ctypes.windll.kernel32.GetOEMCP()
         else:
             enc = (sys.stdout.encoding if sys.stdout.isatty() else
@@ -48,24 +51,24 @@ def setup_console(sys_enc='utf-8', use_colorama=True):
         pass
 
 
-class ColoredHandler( logging.StreamHandler ):
-    def emit( self, record ):
+class ColoredHandler(logging.StreamHandler):
+    def emit(self, record):
         # Need to make a actual copy of the record
         # to prevent altering the message for other loggers
-        myrecord = copy.copy( record )
+        myrecord = copy.copy(record)
         levelno = myrecord.levelno
-        if( levelno >= 50 ): # CRITICAL / FATAL
-            color = '\x1b[31;1m' # red
-        elif( levelno >= 40 ): # ERROR
-            color = '\x1b[31m' # red
-        elif( levelno >= 30 ): # WARNING
-            color = '\x1b[33m' # yellow
-        elif( levelno >= 20 ): # INFO
-            color = '\x1b[32m' # green
-        elif( levelno >= 10 ): # DEBUG
-            color = '\x1b[35m' # pink
-        else: # NOTSET and anything else
-            color = '\x1b[0m' # normal
-        myrecord.msg = (u"%s%s%s" % (color, myrecord.msg, '\x1b[0m')).encode('utf-8')  # normal
-        logging.StreamHandler.emit( self, myrecord )
-        
+        if(levelno >= 50):  # CRITICAL / FATAL
+            color = '\x1b[31;1m'  # red
+        elif(levelno >= 40):  # ERROR
+            color = '\x1b[31m'  # red
+        elif(levelno >= 30):  # WARNING
+            color = '\x1b[33m'  # yellow
+        elif(levelno >= 20):  # INFO
+            color = '\x1b[32m'  # green
+        elif(levelno >= 10):  # DEBUG
+            color = '\x1b[35m'  # pink
+        else:  # NOTSET and anything else
+            color = '\x1b[0m'  # normal
+        myrecord.msg = (u"%s%s%s" % (color, myrecord.msg, '\x1b[0m'))\
+                            .encode('utf-8')  # normal
+        logging.StreamHandler.emit(self, myrecord)
